a720fbde2fcf3ac14c93b573d3bea079d773b109
Ronald Mindiola <ronaldmindiola@live.com>
2025-04-24 13:20:31 -0500

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,16 @@
-
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["standard", "plugin:astro/recommended"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    // Puedes agregar reglas personalizadas aquí
+    "no-unused-vars": "warn",
+    "indent": ["error", 2],
+  },
+};
